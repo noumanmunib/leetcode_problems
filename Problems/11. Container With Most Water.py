d65815f86a29b5d446976ba57b979c900dcf49dc
noumanmunibe <thesmartlad27@gmail.com>
2024-05-29 12:45:44 +0500

    third commit

---
+++ Problems/11. Container With Most Water.py
@@ -0,0 +1,16 @@
+def maxArea(height):
+    res = 0 
+    left, right = 0, len(height) - 1
+
+    while left < right: 
+        area = (right - left) * min(height[left], height[right])
+        res = max(res, area)
+
+        if height[left] < height[right]:
+            left += 1
+        elif height[left] > height[right]:
+            right -= 1
+        else: 
+            right -= 1 # left += 1
+    
+    return res
