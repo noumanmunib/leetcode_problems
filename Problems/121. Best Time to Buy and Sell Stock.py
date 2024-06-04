af167f3de73ddb6c7aa35564b5e1582875d95c10
noumanmunibe <thesmartlad27@gmail.com>
2024-06-05 02:09:37 +0500

    two problems

---
+++ Problems/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,13 @@
+def maxProfit(prices):
+        left, right = 0, 1
+        maxProfit = 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxProfit = max(maxProfit, profit)
+            else: 
+                left = right
+            right += 1
+        
+        return maxProfit
