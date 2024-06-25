3d9dc3637ca6ae5205a626f81961925a50d6f4d9
noumanmunibe <thesmartlad27@gmail.com>
2024-06-25 14:05:39 +0500

    More problems

---
+++ Problems/1004. Max Consecutive Ones III.py
@@ -0,0 +1,13 @@
+def longestOnes(nums, k):
+        left, right = 0, 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                k -= 1
+
+            if k < 0: 
+                if nums[left] == 0:
+                    k += 1            
+                left += 1
+
+        return right - left + 1
