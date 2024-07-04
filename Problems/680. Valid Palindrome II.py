c6279555606601ea01e952649f37a4afd46ec997
noumanmunibe <thesmartlad27@gmail.com>
2024-07-05 02:28:35 +0500

    Leetcode

---
+++ Problems/680. Valid Palindrome II.py
@@ -0,0 +1,15 @@
+def validPalindrome(s):
+        left = 0
+        right = len(s) - 1
+
+        while left <= right:
+                if s[left] != s[right]:
+                    str1 = s[:left] + s[left+1:]
+                    str2 = s[:right] + s[right+1:]
+                    return str1 == str1[::-1] or str2 == str2[::-1]
+                
+                left += 1
+                right -= 1
+            
+        return True
+        
