b504f76a303a287abebf20229b7a49d415604b33
noumanmunibe <thesmartlad27@gmail.com>
2024-08-11 02:08:53 +0500

    LeetCode

---
+++ Problems/441. Arranging Coins.py
@@ -0,0 +1,12 @@
+def arrangeCoins(self, n: int) -> int:
+    l, r = 1, n
+    res = 0
+    while l <=r:
+        mid = (l+r)/
+        coins = (mid /2) * (mid+1)
+        if coins > n:
+            r = mid - 1
+        else:
+            l = mid + 1
+            res = max(mid, res)
+    return res
