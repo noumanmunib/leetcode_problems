6c06031e1c7fad03c42815bbace6a21f4de3421d
noumanmunibe <thesmartlad27@gmail.com>
2024-08-11 14:16:36 +0500

    LeetCode

---
+++ Problems/367. Valid Perfect Square.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        # Using binary search 
+
+        low, high = 1, num
+
+        while low <= high: 
+            mid = (low + high) // 2
+
+            if mid * mid > num: 
+                high = mid - 1
+            elif mid * mid < num: 
+                low = mid + 1
+            else: 
+                return True
+        return False
