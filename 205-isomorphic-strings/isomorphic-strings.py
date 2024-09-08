948e5cb611ab335d7d9ab0f33bae4f2474a8de48
Nouman Munib <113182072+noumanmunib@users.noreply.github.com>
2024-09-08 11:35:12 +0500

    Time: 46 ms (35.67%) | Memory: 16.7 MB (35.32%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        indexS = [0] * 200
+        indexT = [0] * 200
+
+        for i in range(len(s)):
+            if indexS[ord(s[i])] != indexT[ord(t[i])]:
+                return False
+            
+            indexS[ord(s[i])] = i + 1
+            indexT[ord(t[i])] = i + 1
+        
+        return True
