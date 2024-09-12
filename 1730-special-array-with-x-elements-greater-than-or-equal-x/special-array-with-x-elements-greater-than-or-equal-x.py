34fb95552f7d2c9e0440a687e33d6387bcf1c59b
Nouman Munib <113182072+noumanmunib@users.noreply.github.com>
2024-09-12 10:47:35 +0500

    Time: 41 ms (50.06%) | Memory: 16.4 MB (72.36%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -0,0 +1,7 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        for x in range(len(nums) + 1):
+            count = sum(num >= x for num in nums)
+            if count == x:
+                return x
+        return -1
